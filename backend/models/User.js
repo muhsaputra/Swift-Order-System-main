a0e99714c1d3c4fc92a1130351023b54a8254a6b
muhsaputra <muhsaputrabiz@gmail.com>
2026-08-01 00:06:06 +0700

    update user

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,17 +13,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Middleware pre-save untuk otomatis meng-hash password
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   // Jika password tidak diubah, lewati proses hashing
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password")) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    return next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method untuk verifikasi password saat login

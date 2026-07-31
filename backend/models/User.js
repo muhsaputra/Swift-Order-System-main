dc8a26ba1a9eb72772aac3749fa3858aa013b19f
muhsaputra <muhsaputrabiz@gmail.com>
2026-07-31 12:57:43 +0700

    add user

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String, required: true }, // Ditambahkan agar data nama kasir ikut tersimpan
   role: {
     type: String,
     enum: ["cashier", "owner"],

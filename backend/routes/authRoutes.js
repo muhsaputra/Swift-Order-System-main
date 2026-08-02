fc19d2bdfdc53f514d85db1b3787350d8f879669
muhsaputra <muhsaputrabiz@gmail.com>
2026-08-02 19:19:08 +0700

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const Transaction = require("../models/Transaction"); // Model transaksi keuangan untuk laporan
+const Expense = require("../models/Expense"); // Model transaksi keuangan untuk laporan
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const multer = require("multer");
